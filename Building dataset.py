4b86ab7fabd16bea1aa1f22053a2d3f65db0264a
Jannis Niesen <Jannis.nie99@gmail.com>
2022-04-24 18:15:38 +0200

    adding shapefile data

--- Building dataset.py
+++ Building dataset.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 import requests
+import geopandas as gpd
+from shapely.geometry import Polygon, LineString, Point
+
+# settings for printing in console
+width = 320
+pd.set_option("display.width", width)
+np.set_printoptions(linewidth=width)
+pd.set_option("display.max_columns", 30)
+
 # load crawler data
 housing_data = pd.read_excel("housing_data.xlsx")
 
@@ -11,8 +20,8 @@ housing_data["address"] = housing_data.address.str.replace("Auf Karte ansehen", 
 # get lat long from address
 coord_list = []
 
-# get first 2000
-housing_data = housing_data[:2000]
+# subset due to api limitations (2.5k per day)
+housing_data = housing_data[4000:len(housing_data)]
 
 for index, row in housing_data.iterrows():
     link = f"https://api.opencagedata.com/geocode/v1/json?q={row.address}&key=bde3b179622541a9b24545f02697e461"
@@ -27,4 +36,23 @@ for index, row in housing_data.iterrows():
 housing_data["lat"] = [appartement["lat"] for appartement in coord_list]
 housing_data["lon"] = [appartement["lng"] for appartement in coord_list]
 
-housing_data.to_excel("housing_data_cord1.xlsx")
+# saving as xlsx.
+housing_data.to_excel("housing_data_cord1xlsx")
+
+# read in xlsx
+housing_data = pd.read_excel("housing_data_cord.xlsx")
+
+# read in shapefile data
+gemeinde_data = gpd.read_file("vg250_01-01.gk3.shape.ebenen/vg250_ebenen_0101/VG250_GEM.shp")
+
+# get gemeinde key from shapefile data
+gem_key = []
+for index, row in housing_data.iterrows():
+    cord_point = Point(row.lat, row.long)
+    bool_list = gemeinde_data.contains(cord_point)
+
+    bool_index = bool_list.index(True)
+    gem_key.append(gemeinde_data["AGS"][bool_index])
+
+
+
