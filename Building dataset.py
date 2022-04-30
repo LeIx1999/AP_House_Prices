1c9e3955d72091e2f1a7818972447c042863617f
Jannis Niesen <Jannis.nie99@gmail.com>
2022-04-30 15:13:32 +0200

    Everything joined

--- Building dataset.py
+++ Building dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import requests
 import geopandas as gpd
 from shapely.geometry import Polygon, LineString, Point
+import requests
 
 # settings for printing in console
 width = 320
@@ -11,7 +12,7 @@ np.set_printoptions(linewidth=width)
 pd.set_option("display.max_columns", 30)
 
 # load crawler data
-housing_data = pd.read_excel("housing_data.xlsx")
+housing_data = pd.read_excel("data/housing_data.xlsx")
 
 # Cleaning crawling data
 housing_data = housing_data[~housing_data.address.str.contains("Straße nicht freigegeben")]
@@ -21,7 +22,8 @@ housing_data["address"] = housing_data.address.str.replace("Auf Karte ansehen", 
 coord_list = []
 
 # subset due to api limitations (2.5k per day)
-housing_data = housing_data[4000:len(housing_data)]
+# housing_data = housing_data[:2500]
+housing_data = housing_data[:1]
 
 for index, row in housing_data.iterrows():
     link = f"https://api.opencagedata.com/geocode/v1/json?q={row.address}&key=bde3b179622541a9b24545f02697e461"
@@ -37,11 +39,8 @@ for index, row in housing_data.iterrows():
 housing_data["lat"] = [appartement["lat"] for appartement in coord_list]
 housing_data["lon"] = [appartement["lng"] for appartement in coord_list]
 
-# saving as xlsx.
-housing_data.to_excel("data/housing_data_cord1.xlsx")
-
 # read in xlsx
-housing_data = pd.read_excel("data/housing_data_cord.xlsx")
+housing_data = pd.read_excel("data/housing_data_cords.xlsx")
 
 # read in shapefile data
 gemeinde_data = gpd.read_file("data/vg250_01-01.gk3.shape.ebenen/vg250_ebenen_0101/VG250_GEM.shp")
@@ -58,22 +57,23 @@ for index, row in housing_data.iterrows():
     bool_list = gemeinde_data.contains(cord_point)
     try:
         bool_index = bool_list[bool_list == True].index.values[0]
-        gem_key.append(gemeinde_data["AGS"][bool_index])
+        gem_key.append(gemeinde_data["SDV_ARS"][bool_index])
     except:
         gem_key.append("")
 
-housing_data["gem_20"] = gem_key
+housing_data["gemrs_20"] = gem_key
 
 # load regiostar data
 regiostar = pd.read_excel("data/regiostar-referenzdateien.xlsx", sheet_name = "ReferenzGebietsstand2020")
 
 # add leading zero to key
-regiostar["gem_20"] = [f"0{str(x)}" for x in regiostar["gem_20"]]
+regiostar["gemrs_20"] = [f"0{str(x)}" for x in regiostar["gemrs_20"]]
 
 
 # join regiostar
-housing_data = pd.merge(housing_data, regiostar.loc[:, ["gem_20", "RegioStaR7"]], how = "left", on = "gem_20")
+housing_data = pd.merge(housing_data, regiostar.loc[:, ["gemrs_20", "RegioStaR7"]], how = "left", on = "gemrs_20")
 
+# make var explainable
 housing_data["RegioStaR7"] = housing_data["RegioStaR7"].replace({71: "Metropole",
                                                                  72: "Regiopole",
                                                                  73: "Großstadt",
@@ -82,4 +82,20 @@ housing_data["RegioStaR7"] = housing_data["RegioStaR7"].replace({71: "Metropole"
                                                                  76: "Städtischer Raum",
                                                                  77: "Kleinstädtischer, dörflicher Raum"})
 
+# clean df
+housing_data = housing_data.loc[:, "Description":]
 
+# clean gemrs_20
+housing_data["gemrs_20"] = [x[0:9] for x in housing_data["gemrs_20"]]
+
+# Join Bevölkerung
+bev_data = pd.read_excel("data/Gemeindeverzeichnis 2020.xlsx", dtype = {"gem_20": np.object_})
+bev_data = bev_data[~bev_data["Bev_Insgesamt"].isna()]
+
+housing_data = pd.merge(housing_data, bev_data[["gem_20", "Fläche km2 ", "Bev_Insgesamt"]], how = "left", left_on = "gemrs_20", right_on = "gem_20")
+
+# remove duplicates
+housing_data = housing_data.drop_duplicates(subset = ["Description", "Price", "address", "square-meters"])
+
+# add a few variables
+housing_data["Balkon"] = ["Balkon" in x for x in housing_data["information"]]
