db6d4958808c756124a7fd477d73242e20a38497
Jannis Niesen <Jannis.nie99@gmail.com>
2022-05-03 22:43:05 +0200

    Plotting functions

--- Initial EDA.py
+++ Initial EDA.py
@@ -58,13 +58,18 @@ outliers = price_boxplot["fliers"][0].get_data()[1]
 
 housing_data = housing_data[housing_data["price"] <= extreme_outlier]
 
+# Function Histogramm
+def histo(var, xlabel, ylabel, title):
+    plt.hist(var, density=True, bins=20, color="b", edgecolor="k")
+    plt.axvline(var.mean(), color="r", linestyle="dashed")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.tight_layout()
+    plt.show()
+
 # Histogram of price
-plt.hist(housing_data.price, density=True, bins = 20, color = "b", edgecolor = "k")
-plt.axvline(housing_data.price.mean(), color = "r", linestyle = "dashed")
-plt.xlabel("Preis")
-plt.ylabel("Häufigkeit")
-plt.title("Histogramm der abhängigen Variable Preis")
-plt.show()
+histo(housing_data.price, "Preis", "Häufigkeit", "Histogramm der abhängigen Variable Preis")
 
 # Verteilung aller erklärenden Variablen in einem Plot
 
@@ -73,99 +78,50 @@ plt.style.use("default")
 plt.subplot(3, 3, 1)
 housing_data = housing_data[housing_data["square-meters"] != "kA"]
 housing_data["square-meters"] = housing_data["square-meters"].astype("float")
-plt.hist(housing_data["square-meters"], density=True, bins = 20, color = "b", edgecolor = "k")
-plt.axvline(housing_data["square-meters"].mean(), color = "r", linestyle = "dashed")
-plt.xlabel("Quadratmeter")
-plt.ylabel("Häufigkeit")
-plt.title("Histogramm der unabhängigen Variable Quadratmeter")
+histo(housing_data[housing_data["square-meters"] <= 500]["square-meters"], "Quadratmeter", "Häufigkeit", "Histogramm Quadratmeter")
+
+# function bar chart
+def bar(data, var,  xlabel, ylabel, title):
+    df = data.groupby(var)[var].count().to_frame()
+    df = pd.DataFrame({"value": df.index, "percentage": df[var] / len(data)})
+    df = df[df["percentage"] >= 0.001]
+    df = df.sort_values("percentage", ascending=False).reset_index()
+
+    df_plot = sns.barplot(x="value", y="percentage", data=df)
+    for index, value in enumerate(df.percentage):
+        plt.text(index, value,
+                 f"{value:.1%}", ha="center")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    df_plot.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1, decimals=None, symbol='%', is_latex=False))
+    plt.tick_params(axis="x", rotation=45)
+    plt.tight_layout()
 
 # rooms
-rooms = housing_data.groupby("rooms")["rooms"].count().to_frame()
-rooms = pd.DataFrame({"value": rooms.index, "percentage": rooms.rooms / len(housing_data)})
-rooms = rooms[rooms["percentage"] >= 0.001]
-rooms = rooms.sort_values("percentage", ascending = False).reset_index()
-
 plt.subplot(3, 3, 2)
-rooms_plot= sns.barplot(x = "value", y = "percentage", data = rooms)
-for index, value in enumerate(rooms.percentage):
-    plt.text(index, value,
-             f"{value:.1%}", ha = "center")
-plt.xlabel("Räume")
-plt.ylabel("Häufigkeit in Prozent")
-plt.title("Säulendiagramm der unabhängigen Variable Räume")
-rooms_plot.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1, decimals=None, symbol='%', is_latex=False))
-plt.tick_params(axis = "x", rotation = 45)
-plt.tight_layout()
+bar(housing_data, "rooms", "Räume", "Häufigkeit in Prozent", "Säulendiagramm Räume")
 
 # RegioStaR7
-regio = housing_data.groupby("RegioStaR7")["RegioStaR7"].count().to_frame()
-regio = pd.DataFrame({"value": regio.index, "percentage": regio.RegioStaR7 / len(housing_data)})
-regio = regio[regio["percentage"] >= 0.001]
-regio = regio.sort_values("percentage", ascending = False).reset_index()
-
 plt.subplot(3, 3, 3)
-regio_plot = sns.barplot(x = "value", y = "percentage", data = regio)
-for index, value in enumerate(regio.percentage):
-    plt.text(index, value,
-             f"{value:.1%}", ha = "center")
-plt.xlabel("RegioStaR7")
-plt.ylabel("Häufigkeit in Prozent")
-plt.title("Säulendiagramm der unabhängigen Variable RegioStaR7")
-regio_plot.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1, decimals=None, symbol='%', is_latex=False))
-plt.tick_params(axis = "x", rotation = 45)
-plt.tight_layout()
+bar(housing_data, "RegioStaR7", "RegioStaR7", "Häufigkeit in Prozent", "Säulendiagramm RegioStaR7")
 
 plt.subplot(3, 3, 4)
 # gem_size_km2
-plt.hist(housing_data["gem_size_km2"], density=True, bins = 20, color = "b", edgecolor = "k")
-plt.axvline(housing_data["gem_size_km2"].mean(), color = "r", linestyle = "dashed")
-plt.xlabel("Quadratmeter der Gemeinde")
-plt.ylabel("Häufigkeit")
-plt.title("Histogramm der unabhängigen Variable Quadratmeter der Gemeinde")
+histo(housing_data["gem_size_km2"], "Quadratmeter der Gemeinde", "Häufigkeit", "Histogramm Quadratmeter der Gemeinde")
 
 plt.subplot(3, 3, 5)
 # gem_population
-plt.hist(housing_data["gem_population"], density=True, bins = 20, color = "b", edgecolor = "k")
-plt.axvline(housing_data["gem_population"].mean(), color = "r", linestyle = "dashed")
-plt.xlabel("Bevölkerung der Gemeinde")
-plt.ylabel("Häufigkeit")
-plt.title("Histogramm der unabhängigen Variable Bevölerung der Gemeinde")
-plt.tight_layout()
-
+histo(housing_data["gem_population"], "Bevölkerung der Gemeinde", "Häufigkeit", "Histogramm Bevölerung der Gemeinde")
 
 # balcony
-balcony = housing_data.groupby("balcony")["balcony"].count().to_frame()
-balcony = pd.DataFrame({"value": balcony.index, "percentage": balcony.balcony / len(housing_data)})
-balcony = balcony[balcony["percentage"] >= 0.001]
-balcony = balcony.sort_values("percentage", ascending = False).reset_index()
-
 plt.subplot(3, 3, 6)
-balcony_plot = sns.barplot(x = "value", y = "percentage", data = balcony)
-for index, value in enumerate(balcony.percentage):
-    plt.text(index, value,
-             f"{value:.1%}", ha = "center")
-plt.xlabel("Balkon")
-plt.ylabel("Häufigkeit in Prozent")
-plt.title("Säulendiagramm der unabhängigen Variable Balkon")
-balcony_plot.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1, decimals=None, symbol='%', is_latex=False))
-plt.tick_params(axis = "x", rotation = 45)
-plt.tight_layout()
+bar(housing_data, "balcony", "Balkon", "Häufigkeit in Prozent", "Säulendiagramm Balkon")
 
 # floor
-floor = housing_data.groupby("floor")["floor"].count().to_frame()
-floor = pd.DataFrame({"value": floor.index, "percentage": floor.floor / len(housing_data)})
-floor = floor[floor["percentage"] >= 0.001]
-floor = floor.sort_values("percentage", ascending = False).reset_index()
-
 plt.subplot(3, 3, 7)
-floor_plot = sns.barplot(x = "value", y = "percentage", data = floor)
-for index, value in enumerate(floor.percentage):
-    plt.text(index, value,
-             f"{value:.1%}", ha = "center")
-plt.xlabel("Geschoss")
-plt.ylabel("Häufigkeit in Prozent")
-plt.title("Säulendiagramm der unabhängigen Variable Geschoss")
-floor_plot.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1, decimals=None, symbol='%', is_latex=False))
-plt.tick_params(axis = "x", rotation = 45)
-plt.tight_layout()
-plt.show()
+bar(housing_data, "floor", "Geschoss", "Häufigkeit in Prozent", "Säulendiagramm Geschoss")
+
+
+# Plot lat long
+# https://www.earthdatascience.org/tutorials/introduction-to-leaflet-animated-maps/
